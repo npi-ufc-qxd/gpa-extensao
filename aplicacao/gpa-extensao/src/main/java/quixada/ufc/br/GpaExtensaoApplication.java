023c6f039cc353e113f4ecbfe6b61867f7c0e841
Erick Bhrener <erick-bhrener@hotmail.com>
2016-05-13 08:56:03 -0300

    Projeto base criado e adicionado

---
+++ aplicacao/gpa-extensao/src/main/java/quixada/ufc/br/GpaExtensaoApplication.java
@@ -0,0 +1,12 @@
+package quixada.ufc.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpaExtensaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpaExtensaoApplication.class, args);
+	}
+}
