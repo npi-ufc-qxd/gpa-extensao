8ac728ca943195bd92c951dd2e8174807c47ae81
Erick Bhrener <erick-bhrener@hotmail.com>
2016-05-18 11:17:08 -0300

    JNDI configurado para criar tabelas e diagrama de classe atualizado

--- aplicacao/gpa-extensao/src/main/java/ufc/quixada/npi/gpa/config/ServletInitializer.java
+++ aplicacao/gpa-extensao/src/main/java/ufc/quixada/npi/gpa/config/ServletInitializer.java
@@ -1,13 +1,16 @@
 package ufc.quixada.npi.gpa.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
+@EnableAutoConfiguration
+@ComponentScan
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(GpaExtensaoApplication.class);
 	}
-
 }
