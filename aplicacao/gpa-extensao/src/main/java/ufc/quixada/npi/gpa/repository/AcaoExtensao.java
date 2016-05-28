af876235e7560158839a626d55c2cd8a055c4ec2
Virginia Farias <virginia.farias7@gmail.com>
2016-05-28 11:31:17 -0300

    Alteração do nome da interface do rapositório de Ação de Extensão

--- aplicacao/gpa-extensao/src/main/java/ufc/quixada/npi/gpa/repository/AcaoExtensao.java
+++
@@ -1,9 +0,0 @@
-package ufc.quixada.npi.gpa.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AcaoExtensao extends CrudRepository<AcaoExtensao, Long> {
-
-}
---
+++ aplicacao/gpa-extensao/src/main/java/ufc/quixada/npi/gpa/repository/AcaoExtensaoRepository.java
@@ -0,0 +1,11 @@
+package ufc.quixada.npi.gpa.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import ufc.quixada.npi.gpa.model.AcaoExtensao;
+
+@Repository
+public interface AcaoExtensaoRepository extends CrudRepository<AcaoExtensao, Long> {
+
+}
