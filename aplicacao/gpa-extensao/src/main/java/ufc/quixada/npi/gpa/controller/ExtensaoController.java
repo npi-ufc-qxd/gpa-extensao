b17da41ebaf4b36f2e5b0f226c14406f2456adc2
04297231301 <04297231301@npi-0345896>
2016-05-31 12:01:19 -0300

    Recebendo todas as ações do banco que possuem o coordenador logado e mostrando em uma tabela, adição de cards e arquivo js para adicionar script jQuery da página.

--- aplicacao/gpa-extensao/src/main/java/ufc/quixada/npi/gpa/controller/ExtensaoController.java
+++ aplicacao/gpa-extensao/src/main/java/ufc/quixada/npi/gpa/controller/ExtensaoController.java
@@ -1,7 +1,18 @@
 package ufc.quixada.npi.gpa.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ufc.quixada.npi.gpa.model.AcaoExtensao.Status;
+import ufc.quixada.npi.gpa.model.Pessoa;
+import ufc.quixada.npi.gpa.repository.AcaoExtensaoRepository;
+import ufc.quixada.npi.gpa.repository.PessoaRepository;
 
 @Controller
 public class ExtensaoController {
@@ -10,5 +21,16 @@ public class ExtensaoController {
 	public String index() {
 		return "index";
 	}
-
+	@Autowired
+	private AcaoExtensaoRepository acaoExtensaoRepository;
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	@RequestMapping("/listagem")
+	public String listagem(Model model, Authentication authentication) {
+		Pessoa pessoa = pessoaRepository.getByCpf(authentication.getName());
+		//"NOVO", "AGUARDANDO PARECERISTA"
+		List<Status> status = Arrays.asList(Status.values());
+		model.addAttribute("acoes", acaoExtensaoRepository.findByCoordenadorAndStatusIn(pessoa, status));
+		return "coordenacao/listagem/listagem";
+	}
 }
