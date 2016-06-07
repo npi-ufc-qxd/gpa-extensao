68ec922f0481ad335b3c446313bee022e4fa6a1a
04297231301 <04297231301@npi-0345896>
2016-06-07 11:58:35 -0300

    Adição de cards de parecer e participação, mudança de classes dos cards, adição de mudança na cor do background da linha da tabela em casa que o status do projeto esteja resolvendo pendências.

--- aplicacao/gpa-extensao/src/main/resources/static/js/listagem.js
+++ aplicacao/gpa-extensao/src/main/resources/static/js/listagem.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
 	$(".homologados").hide();
 	$(".card").click(function() {
         $(".card").find("i").removeClass("fa-folder-open-o");
-        $(".card").find(".small-box").removeClass("bg-aqua");
+        $(".card").find(".small-box").removeClass("bg-blue");
 
         $(".card").find("i").addClass("fa-folder-o");
-        $(".card").find(".small-box").addClass("bg-yellow");
+        $(".card").find(".small-box").addClass("bg-aqua");
 
         $(this).find("i").toggleClass("fa-folder-open-o fa-folder-o");
-        $(this).find(".small-box").toggleClass("bg-aqua bg-yellow");
+        $(this).find(".small-box").toggleClass("bg-blue bg-aqua");
     });
 	$(".table-acoes").DataTable({
 		"filter" : false
@@ -18,9 +18,27 @@ function showNaoHomologados() {
 	$(".tramitacao").show();
 	$(".novos").show();
 	$(".homologados").hide();
+	$(".parecer").hide();
+	$(".participacao").hide();
 }
 function showHomologados() {
 	$(".tramitacao").hide();
 	$(".novos").hide();
 	$(".homologados").show();
+	$(".parecer").hide();
+	$(".participacao").hide();
+}
+function showParecer() {
+	$(".tramitacao").hide();
+	$(".novos").hide();
+	$(".homologados").hide();
+	$(".parecer").show();
+	$(".participacao").hide();
+}
+function showParticipacao() {
+	$(".tramitacao").hide();
+	$(".novos").hide();
+	$(".homologados").hide();
+	$(".parecer").hide();
+	$(".participacao").show();
 }
