45f59cb248dd4dee5b7f6c6175cf4cc86b8c653b
Marcilio Santos Valois <marciliovalois@hotmail.com>
2016-06-13 11:51:53 -0300

    Adicionada a funcionalidade de editar os campos de número de bolsas recebidas e código da ação após a mesma ser aprovada.

--- aplicacao/gpa-extensao/src/main/resources/static/js/detalheAcao.js
+++ aplicacao/gpa-extensao/src/main/resources/static/js/detalheAcao.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
     $("#input-codigo").hide();
     $("#input-bolsas").hide();
-    $("#salvar-codigo").hide();
-    $("#salvar-bolsas").hide();
+   
 	
 	$("#editar-codigo").click(function(){
         $("#input-codigo").show();
         $("#codigo-acao").hide();
         $("#editar-codigo").hide();
-        $("#salvar-codigo").show();
+
         
     });
 	
@@ -16,22 +15,7 @@ $(document).ready(function(){
 		$("#input-bolsas").show();
 		$("#bolsas-recebidas").hide();
 		$("#editar-bolsas").hide();
-		$("#salvar-bolsas").show();
+
 	});
 	
-	$("#salvar-codigo").click(function(){
-		$("#salvar-codigo").hide();
-		$("#input-codigo").hide();
-		$("#editar-codigo").show();
-		$("#codigo-acao").show();
-		
-	});
-	
-	$("#salvar-bolsas").click(function(){
-		$("#salvar-bolsas").hide();
-		$("#input-bolsas").hide();
-		$("#editar-bolsas").show();
-		$("#bolsas-recebidas").show();
-		
-	});
 });
