dcd46ebd35b4928a01c64a1cf0ceb6ce91695755
Ruben Blenicio Tavares Silva <blenicio777650@gmail.com>
2016-06-13 08:43:35 -0300

    Correções de erros após merge com o master.

--- aplicacao/gpa-extensao/src/main/java/ufc/quixada/npi/gpa/util/Constants.java
+++ aplicacao/gpa-extensao/src/main/java/ufc/quixada/npi/gpa/util/Constants.java
@@ -24,7 +24,7 @@ public class Constants {
 	public static final String ACOES_AGUARDANDO_RELATOR = "acoesAguardandoRelator";
 	public static final String ACOES_AGUARDANDO_HOMOLOGACAO = "acoesAguardandoHomologacao";
 	public static final String ACOES_HOMOLOGADAS = "acoesHomologadas";
-	public static final String ACAO_EXTENSAO = "acaoextensao";
+	public static final String ACAO_EXTENSAO = "acaoExtensao";
 	public static final String PERMISSAO_COORDENADOR = "coordenador";
 	public static final String PERMISSAO_DIRETOR = "diretor";
 	public static final String PERMISSAO_PARECERISTA = "parecerista";
