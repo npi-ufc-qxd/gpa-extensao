8740edc56b2cdb0d6bbd19827125b28bc0248d5f
felypetabosa <felypetabosa@gmail.com>
2016-06-14 10:23:12 -0300

    Implementando a validação dos campos de data Finalizando a tarefa

--- aplicacao/gpa-extensao/src/main/resources/static/js/script.js
+++ aplicacao/gpa-extensao/src/main/resources/static/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	//$("#cadastrar").attr("disabled","disabled");
 	$("#message").hide();
 	$("#cargaHorarias").hide();
+	
     $('#dataInicio,#dataTermino').datepicker({
         format: "dd/mm/yyyy",
         maxViewMode: 2,
@@ -10,22 +11,39 @@ $(document).ready(function() {
         autoclose: true
     });
     
-    /*$("#dataTermino").change(function() {
+    var verificarData = function(Inicio,Termino){
     	var dataInicio = new Object();
     	var dataTermino = new Object();
-    	dataInicio.dia = +$("#dataInicio").val().substring(0,2);
-    	dataInicio.mes = +$("#dataInicio").val().substring(3,5);
-    	dataInicio.ano = +$("#dataInicio").val().substring(6);
-    	dataInicio.dia = +$("#dataTermino").val().substring(0,2);
-    	dataInicio.mes = +$("#dataTermino").val().substring(3,5);
-    	dataInicio.ano = +$("#dataTermino").val().substring(6);
-    	
-    	
-    	alert(dataInicio.dia + " " + dataInicio.mes + " " + dataInicio.ano);
-    	alert(typeof dataInicio.dia);
+    	dataInicio.dia = +Inicio.val().substring(0,2);
+    	dataInicio.mes = +Inicio.val().substring(3,5);
+    	dataInicio.ano = +Inicio.val().substring(6);
+    	dataTermino.dia = +Termino.val().substring(0,2);
+    	dataTermino.mes = +Termino.val().substring(3,5);
+    	dataTermino.ano = +Termino.val().substring(6);
+    	dataInicio.Total = (dataInicio.dia)+((dataInicio.mes)*30)+((dataInicio.ano)*365);
+		dataTermino.Total = (dataTermino.dia)+((dataTermino.mes)*30)+((dataTermino.ano)*365);
+		if(dataTermino.Total >= dataInicio.Total){
+			return true;
+    	}else{
+    		return false;
+    	}
+	};
+	
+    $("#dataTermino").change(function() {
+    	var dataValida = verificarData($("#dataInicio"),$("#dataTermino"));
+    	if(!dataValida){
+    		$("#message").removeClass("alert alert-success alert-dismissible");
+    		$("#message").addClass("alert alert-danger alert-dismissible");
+    		$("#message").html("A data de início não pode ser posterior a data de Término.").slideDown("slow");
+    		
+    	}else{
+    		$("#message").removeClass("alert alert-danger alert-dismissible");
+    		$("#message").addClass("alert alert-success alert-dismissible");
+    		$("#message").html("Data Válida.").slideUp("slow");
+    	}
     });
-    */
-	$("#message").addClass("alert alert-danger alert-dismissible");   
+    
+	   
 	
 	$("#modalidade").change(function(){
 			var selected = $(this).find(":selected").val();
@@ -44,4 +62,6 @@ $(document).ready(function() {
 				$("#cargaHorarias").slideUp("slow");
 			}
 	});
+	
+	
 });
