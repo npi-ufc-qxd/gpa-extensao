c30e1c01e5b6294aa5bba8ae0a432e324025bd59
PauloNobre <paulinho140@gmail.com>
2016-06-24 10:00:44 -0300

    Inserir quantidade de bolsas recebidas , com participantes a definir

--- aplicacao/gpa-extensao/src/main/java/ufc/quixada/npi/gpa/repository/ParticipacaoRepository.java
+++ aplicacao/gpa-extensao/src/main/java/ufc/quixada/npi/gpa/repository/ParticipacaoRepository.java
@@ -2,14 +2,20 @@ package ufc.quixada.npi.gpa.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import ufc.quixada.npi.gpa.model.AcaoExtensao;
 import ufc.quixada.npi.gpa.model.Participacao;
+import ufc.quixada.npi.gpa.model.Participacao.Funcao;
 
 @Repository
 public interface ParticipacaoRepository extends CrudRepository<Participacao, Integer> {
 
 	public List<Participacao> findByAcaoExtensao(AcaoExtensao acao);
+	
+	//@Query("select COUNT(*) FROM AcaoExtensao as ae WHERE ae.id =:id AND ae.equipe_de_trabalho.funcao =:funcao")
+	public List<Participacao> findByAcaoExtensaoAndFuncao(AcaoExtensao acao, Funcao funcao);
 }
