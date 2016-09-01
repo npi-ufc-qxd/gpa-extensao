8e2cd86d41b91cab61391ab6c8af9cd6ef6f669e
PauloNobre <paulinho140@gmail.com>
2016-09-01 11:55:35 -0300

    criar paginas de erro 404 e 403;
    redirecionar os erros para suas respectivas paginas
    
    concertar falha na segurança, pois todos tinham acesso as urls de admin

--- aplicacao/gpa-extensao/src/main/java/ufc/quixada/npi/gpa/config/MvcConfig.java
+++ aplicacao/gpa-extensao/src/main/java/ufc/quixada/npi/gpa/config/MvcConfig.java
@@ -1,6 +1,10 @@
 package ufc.quixada.npi.gpa.config;
 
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -11,4 +15,11 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
     }
+	
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+		return (container ->
+		container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404"),
+								new ErrorPage(HttpStatus.FORBIDDEN, "/403")));
+	}
 }
