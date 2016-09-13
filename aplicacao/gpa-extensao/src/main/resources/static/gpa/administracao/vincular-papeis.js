b884b51b36a7ea511f661f766ef2ddef4b98d89d
PauloNobre <paulinho140@gmail.com>
2016-09-13 09:38:12 -0300

    remover tokens

--- aplicacao/gpa-extensao/src/main/resources/static/gpa/administracao/vincular-papeis.js
+++ aplicacao/gpa-extensao/src/main/resources/static/gpa/administracao/vincular-papeis.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-	var token = $("meta[name='_csrf']").attr("content");
-	var header = $("meta[name='_csrf_header']").attr("content");
 
 	$("#select-pessoa, #select-papeis-atuais").select2();
 
