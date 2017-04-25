a2cd3c3728da4c94561ff809414cba4bacb7474c
Marcelo Henrique Pereira <marcelo12tab@gmail.com>
2017-04-25 09:52:57 -0300

    Melhorias vistas no Codacy

--- aplicacao/gpa-extensao/src/main/java/ufc/quixada/npi/gpa/service/impl/ParticipacaoServiceImpl.java
+++ aplicacao/gpa-extensao/src/main/java/ufc/quixada/npi/gpa/service/impl/ParticipacaoServiceImpl.java
@@ -69,64 +69,58 @@ public class ParticipacaoServiceImpl implements ParticipacaoService {
 		AcaoExtensao old = acaoExtensaoRepository.findOne(acaoExtensao.getId());
 		if (old != null) {
 
-			if (acaoExtensao != null) {
-
-				if (participacao.getParticipante() != null) {
-					participacao.setCpfParticipante(participacao.getParticipante().getCpf());
-					participacao.setNomeParticipante(participacao.getParticipante().getNome());
-				}
-				if (!old.getStatus().equals(Status.NOVO)
-						&& !old.getStatus().equals(Status.RESOLVENDO_PENDENCIAS_PARECER)
-						&& !old.getStatus().equals(Status.RESOLVENDO_PENDENCIAS_RELATO)
-						&& !old.getStatus().equals(Status.APROVADO)) {
-					throw new GpaExtensaoException(ERROR_ADICIONAR_PARTICIPANTE_NAO_PERMITIDO);
-				}
-
-				if (participacao.getDataInicio() == null || participacao.getDataTermino() == null
-						|| participacao.getDataInicio().before(old.getInicio())
-						|| participacao.getDataTermino().after(old.getTermino())
-						|| participacao.getDataInicio().after(old.getTermino())
-						|| participacao.getDataTermino().before(old.getInicio())
-						|| participacao.getDataTermino().before(participacao.getDataInicio())) {
-					throw new GpaExtensaoException(EXCEPTION_DATA_INVALIDA);
-				}
-
-				participacao.setCoordenador(false);
-				participacao.setAcaoExtensao(acaoExtensao);
-
-				if (!acaoExtensao.getCoordenador().getCpf().equalsIgnoreCase(coordenador.getCpf())) {
-					throw new GpaExtensaoException(MENSAGEM_PERMISSAO_NEGADA);
-				} else if (participacao.getFuncao().equals(Funcao.OUTRA)) {
-					if (participacao.getDescricaoFuncao().replaceAll(" ", "").isEmpty()
-							|| participacao.getCpfParticipante().replaceAll(" ", "").isEmpty()
-							|| participacao.getNomeParticipante().replaceAll(" ", "").isEmpty()) {
-						throw new GpaExtensaoException(VALOR_INVALIDO);
-					}
-
-				} else if (!participacao.getInstituicao().equals(Instituicao.UFC)) {
-					if (participacao.getNomeInstituicao().replaceAll(" ", "").isEmpty()) {
-						throw new GpaExtensaoException(VALOR_INVALIDO);
-					}
-
-				} else if (participacao.getCargaHoraria() < 4 || participacao.getCargaHoraria() > 16) {
-					throw new GpaExtensaoException(ERROR_QTD_HORAS_NAO_PERMITIDA);
-				} else if (participacao.getParticipante() != null) {
-					Servidor servidor = servidorRepository.findByPessoa_cpf(participacao.getCpfParticipante());
-					if ((servidor.getDedicacao().equals(Dedicacao.H20))) {
-						if (participacao.getCargaHoraria() > 12) {
-							throw new GpaExtensaoException(ERROR_QTD_HORAS_NAO_PERMITIDA);
-						}
-					}
-
-				}
-				for (Participacao p : acaoExtensao.getEquipeDeTrabalho()) {
-					if (p.getCpfParticipante().equalsIgnoreCase(participacao.getCpfParticipante())) {
-						throw new GpaExtensaoException(ERROR_PESSOA_JA_PARTICIPANTE);
-					}
-				}
-				acaoExtensao.getEquipeDeTrabalho().add(participacao);
-				acaoExtensaoRepository.save(acaoExtensao);
+			if (participacao.getParticipante() != null) {
+				participacao.setCpfParticipante(participacao.getParticipante().getCpf());
+				participacao.setNomeParticipante(participacao.getParticipante().getNome());
 			}
+			if (!old.getStatus().equals(Status.NOVO) && !old.getStatus().equals(Status.RESOLVENDO_PENDENCIAS_PARECER)
+					&& !old.getStatus().equals(Status.RESOLVENDO_PENDENCIAS_RELATO)
+					&& !old.getStatus().equals(Status.APROVADO)) {
+				throw new GpaExtensaoException(ERROR_ADICIONAR_PARTICIPANTE_NAO_PERMITIDO);
+			}
+
+			if (participacao.getDataInicio() == null || participacao.getDataTermino() == null
+					|| participacao.getDataInicio().before(old.getInicio())
+					|| participacao.getDataTermino().after(old.getTermino())
+					|| participacao.getDataInicio().after(old.getTermino())
+					|| participacao.getDataTermino().before(old.getInicio())
+					|| participacao.getDataTermino().before(participacao.getDataInicio())) {
+				throw new GpaExtensaoException(EXCEPTION_DATA_INVALIDA);
+			}
+
+			participacao.setCoordenador(false);
+			participacao.setAcaoExtensao(acaoExtensao);
+
+			if (!acaoExtensao.getCoordenador().getCpf().equalsIgnoreCase(coordenador.getCpf())) {
+				throw new GpaExtensaoException(MENSAGEM_PERMISSAO_NEGADA);
+			} else if (participacao.getFuncao().equals(Funcao.OUTRA)
+					&& (participacao.getDescricaoFuncao().replaceAll(" ", "").isEmpty()
+							|| participacao.getCpfParticipante().replaceAll(" ", "").isEmpty()
+							|| participacao.getNomeParticipante().replaceAll(" ", "").isEmpty())) {
+
+				throw new GpaExtensaoException(VALOR_INVALIDO);
+
+			} else if (!participacao.getInstituicao().equals(Instituicao.UFC)) {
+				if (participacao.getNomeInstituicao().replaceAll(" ", "").isEmpty()) {
+					throw new GpaExtensaoException(VALOR_INVALIDO);
+				}
+
+			} else if (participacao.getCargaHoraria() < 4 || participacao.getCargaHoraria() > 16) {
+				throw new GpaExtensaoException(ERROR_QTD_HORAS_NAO_PERMITIDA);
+			} else if (participacao.getParticipante() != null) {
+				Servidor servidor = servidorRepository.findByPessoa_cpf(participacao.getCpfParticipante());
+				if ((servidor.getDedicacao().equals(Dedicacao.H20)) && participacao.getCargaHoraria() > 12) {
+					throw new GpaExtensaoException(ERROR_QTD_HORAS_NAO_PERMITIDA);
+				}
+
+			}
+			for (Participacao p : acaoExtensao.getEquipeDeTrabalho()) {
+				if (p.getCpfParticipante().equalsIgnoreCase(participacao.getCpfParticipante())) {
+					throw new GpaExtensaoException(ERROR_PESSOA_JA_PARTICIPANTE);
+				}
+			}
+			acaoExtensao.getEquipeDeTrabalho().add(participacao);
+			acaoExtensaoRepository.save(acaoExtensao);
 		}
 
 	}
