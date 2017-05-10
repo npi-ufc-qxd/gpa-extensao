dd1e7440271e53239b77343636db594cf4abbc0b
Leticia Aguiar <leticia.aad@gmail.com>
2017-05-10 07:01:44 -0300

    Adiciona sweetalert para testar excluir equipe de trabalho

--- aplicacao/gpa-extensao/src/main/resources/static/js/jquery.sweet-alert.custom.js
+++ aplicacao/gpa-extensao/src/main/resources/static/js/jquery.sweet-alert.custom.js
@@ -36,7 +36,22 @@
             swal("Deleted!", "Your imaginary file has been deleted.", "success"); 
         });
     });
-
+    
+    $('.sa-warning').click(function(){
+        swal({   
+            title: "Are you sure?",   
+            text: "You will not be able to recover this imaginary file!",   
+            type: "warning",   
+            showCancelButton: true,   
+            confirmButtonColor: "#DD6B55",   
+            confirmButtonText: "Yes, delete it!",   
+            closeOnConfirm: false 
+        }, function(){   
+            swal("Deleted!", "Your imaginary file has been deleted.", "success"); 
+        });
+    });
+    
+    
     //Parameter
     $('#sa-params').click(function(){
         swal({   
