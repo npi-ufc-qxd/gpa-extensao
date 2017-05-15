4b3104fa452aeceb372f9c9e3dc68dacce092ce8
06342550379 <06342550379@npi-345904>
2017-05-15 11:36:51 -0300

    Removendo imports não utilizados

--- aplicacao/gpa-extensao/src/main/java/ufc/quixada/npi/gpa/controller/AcaoExtensaoController.java
+++ aplicacao/gpa-extensao/src/main/java/ufc/quixada/npi/gpa/controller/AcaoExtensaoController.java
@@ -30,8 +30,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpRequest;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
