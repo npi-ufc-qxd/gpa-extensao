2e4554baa79d9c0de581eb4e18b228ab676ed4d9
06342550379 <06342550379@npi-345904>
2017-06-06 08:47:36 -0300

    Adicionado funcionalidade para não permitir datas inválidas na página de visisualização da ação.

--- aplicacao/gpa-extensao/src/main/resources/static/js/extensao.js
+++ aplicacao/gpa-extensao/src/main/resources/static/js/extensao.js
@@ -5,7 +5,7 @@ function validar (input, element) {
 	var dia = resData[0];
 	var mes = resData[1];
 	var ano = resData[2];
-
+	
 	if(dia == "__" || mes == "__" || ano == "____") {
 		input.val("");
 		element.addClass('no-display');
@@ -17,35 +17,38 @@ function validar (input, element) {
 		var parseDia = parseInt(dia);
 		var parseMes = parseInt(mes);
 		
-		if(parseDia >= 32) {
-			input.val("");
-			element.text("Dia inválido");
-			element.removeClass('no-display');
-		}else if(parseMes <= 12) {
+		if(parseMes <= 12) {
 			if(parseMes == 1 || parseMes == 3 || parseMes == 5 || parseMes == 7 || parseMes == 8 
 					|| parseMes == 10 || parseMes == 12) {
-				if(parseDia < 31) {
+				if(parseDia > 31) {
 					input.val("");
 					element.text("Esse mês possui 31 dias");
 					element.removeClass('no-display');
+				}else {
+					element.addClass('no-display');
 				}
 			} else if(parseMes == 4 || parseMes == 6 || parseMes == 9 || parseMes == 11) {
-				if(parseDia < 30) {
+				if(parseDia > 30) {
 					input.val("");
 					element.text("Esse mês possui 30 dias");
 					element.removeClass('no-display');
+				}else {
+					element.addClass('no-display');
 				}
 			} else if(parseMes == 2){
 				if(parseDia > 29) {
 					input.val("");
 					element.text("Esse mês possui 28 ou 29 dias");
 					element.removeClass('no-display');
+				}else {
+					element.addClass('no-display');
 				}
-			}
-		} else {
+			} 
+		} else if(parseMes > 12){
 			input.val("");
 			element.text("Esse mês não existe");
 			element.removeClass('no-display');
-		}
+		} 
 	}
+	
 }
